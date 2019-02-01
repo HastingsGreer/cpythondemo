e39f3dd4047f52ccc2f19031b8d224d0219da493
HastingsGreer <hastings.greer@kitware.com>
2019-02-01 09:44:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy
 module1 = Extension('spam',
                     sources = ['spammodule.c'],
                     include_dirs=[numpy.get_include()],
-                    extra_compile_args = ["/openmp"])
+                    extra_compile_args = [])
 
 setup (name = 'SpamModule',
        version = '1.0',
